d22d105f84cb402dc0bed89049550e3155a5e006
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2025-11-09 10:48:27 +0000

    Add files via upload

--- Stopwatch.py
+++ Stopwatch.py
@@ -11,21 +11,23 @@ heading.pack()
 hours = tk.StringVar(value="00")
 minutes = tk.StringVar(value="00")
 seconds = tk.StringVar(value="00")
+milliseconds = tk.StringVar(value="00")
 
 isPaused = False
 isRunning = False
-elapsedTime = 0
+elapsedTime = 0 
 
 def updateDisplay():
     global elapsedTime
-    mins, secs = divmod(elapsedTime, 60)
-    hrs = 0
-    if mins >= 60:
-        hrs, mins = divmod(mins, 60)
+    total_ms = elapsedTime
+    total_secs, ms = divmod(total_ms, 1000)
+    mins, secs = divmod(total_secs, 60)
+    hrs, mins = divmod(mins, 60)
 
-    hours.set(f"{hrs:02d}")
-    minutes.set(f"{mins:02d}")
-    seconds.set(f"{secs:02d}")
+    hours.set(f"{hrs : 02d}")
+    minutes.set(f"{mins : 02d}")
+    seconds.set(f"{secs : 02d}")
+    milliseconds.set(f"{ms//10 : 02d}")
 
 def runStopwatch():
     global elapsedTime, isPaused, isRunning
@@ -35,8 +37,8 @@ def runStopwatch():
     while isRunning and not isPaused:
         updateDisplay()
         window.update()
-        time.sleep(1)
-        elapsedTime += 1
+        time.sleep(0.01)
+        elapsedTime += 10
 
 def startStopwatch():
     global isRunning
@@ -53,15 +55,17 @@ def resumeStopwatch():
         isPaused = False
         runStopwatch()
 
-
 h = tk.Entry(window, font=("Verdana", 40), bg="#dad7cd", justify='center', width=5, textvariable=hours, state="disabled")
-h.place(x=125, y=150)
+h.place(x=15, y=150)
 
 m = tk.Entry(window, font=("Verdana", 40), bg="#dad7cd", justify='center', width=5, textvariable=minutes, state="disabled")
-m.place(x=325, y=150)
+m.place(x=215, y=150)
 
 s = tk.Entry(window, font=("Verdana", 40), bg="#dad7cd", justify='center', width=5, textvariable=seconds, state="disabled")
-s.place(x=525, y=150)
+s.place(x=415, y=150)
+
+ms = tk.Entry(window, font=("Verdana", 40), bg="#dad7cd", justify='center', width=5, textvariable=milliseconds, state="disabled")
+ms.place(x=615, y=150)
 
 startBtn = tk.Button(window, text="Start", font=("Verdana", 20), bg="#dad7cd", width=10, command=startStopwatch)
 startBtn.place(x=325, y=300)
